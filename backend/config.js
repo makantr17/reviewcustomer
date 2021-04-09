e80e663cec9609915841e1340e5ac9322553bbee
makantr17 <makant17@alustudent.com>
2021-04-09 20:20:10 +0200

    make it betterd

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 export default{
-    // MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://node-shop:node-shop@node-rest-shop.ja4f9.mongodb.net/shopBackend?retryWrites=true&w=majority",
-    MONGODB_URL: process.env.MONGODB_URL || "mongodb://localhost/entrepriseMS",
+    MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://node-shop:node-shop@node-rest-shop.ja4f9.mongodb.net/shopBackend?retryWrites=true&w=majority",
+    // MONGODB_URL: process.env.MONGODB_URL || "mongodb://localhost/entrepriseMS",
     JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
     JWT_COMPANY_SECRET: process.env.JWT_COMPANY_SECRET || 'companylogs',
     JWT_SECRET_REGISTER:  process.env.JWT_SECRET_REGISTER || 'http:localhose',
